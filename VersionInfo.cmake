91f3dc404d23413ed8bf9ad980e0e966085fa644
Scott Aron Bloom <scott@towel42.com>
2021-12-23 20:51:23 +0000

    Rev Version

--- VersionInfo.cmake
+++ VersionInfo.cmake
@@ -21,7 +21,7 @@
 # SOFTWARE.
 
 SET( MAJOR_VERSION 0 )
-SET( MINOR_VERSION 14 )
+SET( MINOR_VERSION 15 )
 SET( APP_NAME "Media Manager" )
 SET( VENDOR   "Scott Aron Bloom" )
 SET( HOMEPAGE "www.towel42.com" )
