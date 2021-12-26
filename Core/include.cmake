f278c9b3b4405c766a87ad92d01d3102815aa5cf
Scott Aron Bloom <scott@towel42.com>
2021-12-26 06:16:35 +0000

    Update Copyright

--- Core/include.cmake
+++ Core/include.cmake
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2020 Scott Aron Bloom
+# Copyright (c) 2020-2021 Scott Aron Bloom
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
