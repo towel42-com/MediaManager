2d2ee7889471ab8ec09b9003cceae952633842e7
Scott Aron Bloom <scott@towel42.com>
2022-02-03 23:49:36 +0000

    Make SRT -> Merge SRT

--- UI/MainWindow.cpp
+++ UI/MainWindow.cpp
@@ -190,7 +190,7 @@ namespace NMediaManager
         void CMainWindow::addPages()
         {
             addPage( std::make_shared< STabDef >( new CMakeMKVPage( nullptr ), tr( "Make MKV" ), QString::fromUtf8( ":/mkv.png" ), fImpl->tabWidget ) );
-            addPage( std::make_shared< STabDef >( new CMergeSRTPage( nullptr ), tr( "Make SRT" ), QString::fromUtf8( ":/cc.png" ), fImpl->tabWidget ) );
+            addPage( std::make_shared< STabDef >( new CMergeSRTPage( nullptr ), tr( "Merge SRT" ), QString::fromUtf8( ":/cc.png" ), fImpl->tabWidget ) );
             addPage( std::make_shared< STabDef >( new CTransformPage( nullptr ), tr( "Media Namer" ), QString::fromUtf8( ":/rename.png" ), fImpl->tabWidget ) );
             addPage( std::make_shared< STabDef >( new CTagsPage( nullptr ), tr( "Media Tags" ), QString::fromUtf8( ":/tag.png" ), fImpl->tabWidget ) );
             auto bifPage = addPage( std::make_shared< STabDef >( new CBIFViewerPage( nullptr ), tr( "BIF Viewer" ), QString::fromUtf8( ":/roku.png" ), fImpl->tabWidget ) );
