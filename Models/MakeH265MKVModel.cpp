97fe8d232c20bd53a96c3369a3b58c05219a9ef9
Scott Aron Bloom <scott@towel42.com>
2023-03-05 18:23:41 -0800

    Update UI to show where to download external tools MakeMKV is now Make H265 MKV

---
+++ Models/MakeH265MKVModel.cpp
@@ -0,0 +1,140 @@
+// The MIT License( MIT )
+//
+// Copyright( c ) 2020-2021 Scott Aron Bloom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "MakeH265MKVModel.h"
+#include "Preferences/Core/Preferences.h"
+#include "SABUtils/FileUtils.h"
+#include "SABUtils/DoubleProgressDlg.h"
+#include "SABUtils/MKVUtils.h"
+
+#include <QDir>
+#include <QTimer>
+
+namespace NMediaManager
+{
+    namespace NModels
+    {
+        CMakeH265MKVModel::CMakeH265MKVModel( NUi::CBasePage *page, QObject *parent /*= 0*/ ) :
+            CDirModel( page, parent )
+        {
+        }
+
+        CMakeH265MKVModel::~CMakeH265MKVModel()
+        {
+        }
+
+        QStringList CMakeH265MKVModel::dirModelFilter() const
+        {
+            return NPreferences::NCore::CPreferences::instance()->getNonMKVMediaExtensions();
+        }
+
+        std::pair< bool, QStandardItem * > CMakeH265MKVModel::processItem( const QStandardItem *item, bool displayOnly )
+        {
+            if ( item->data( ECustomRoles::eIsDir ).toBool() )
+                return std::make_pair( true, nullptr );
+
+            SProcessInfo processInfo;
+            processInfo.fSetMKVTagsOnSuccess = true;
+            processInfo.fOldName = item->data( ECustomRoles::eAbsFilePath ).toString();
+            auto fi = QFileInfo( processInfo.fOldName );
+            processInfo.fNewNames << fi.absoluteDir().absoluteFilePath( fi.completeBaseName() + ".mkv" );
+            processInfo.fItem = new QStandardItem( QString( "Convert '%1' => '%2'" ).arg( getDispName( processInfo.fOldName ) ).arg( getDispName( processInfo.fNewNames.front() ) ) );
+            processInfo.fItem->setData( processInfo.fOldName, ECustomRoles::eOldName );
+            processInfo.fItem->setData( processInfo.fNewNames, ECustomRoles::eNewName );
+
+            bool aOK = true;
+            QStandardItem *myItem = nullptr;
+            fFirstProcess = true;
+            if ( !displayOnly )
+            {
+                processInfo.fMaximum = NSABUtils::getNumberOfSeconds( processInfo.fOldName );
+                processInfo.fCmd = NPreferences::NCore::CPreferences::instance()->getFFMpegEXE();
+                if ( processInfo.fCmd.isEmpty() || !QFileInfo( processInfo.fCmd ).isExecutable() )
+                {
+                    QStandardItem *errorItem = nullptr;
+                    if ( processInfo.fCmd.isEmpty() )
+                        appendError( processInfo.fItem, tr( "ffmpeg is not set properly" ) );
+                    else
+                        appendError( processInfo.fItem, tr( "ffmpeg '%1' is not an executable" ).arg( processInfo.fCmd ) );
+                    aOK = false;
+                }
+
+                aOK = aOK && checkProcessItemExists( processInfo.fOldName, processInfo.fItem );
+                processInfo.fTimeStamps = NSABUtils::NFileUtils::timeStamps( processInfo.fOldName );
+
+                processInfo.fArgs = QStringList() << "-y"
+                                                  << "-fflags"
+                                                  << "+genpts"
+                                                  << "-i" << processInfo.fOldName << "-c:v"
+                                                  << "copy"
+                                                  << "-c:a"
+                                                  << "copy" << processInfo.fNewNames;
+                fProcessQueue.push_back( processInfo );
+                QTimer::singleShot( 0, this, &CDirModel::slotRunNextProcessInQueue );
+            }
+            myItem = processInfo.fItem;
+            return std::make_pair( aOK, myItem );
+        }
+
+        QString CMakeH265MKVModel::getProgressLabel( const SProcessInfo &processInfo ) const
+        {
+            auto retVal = QString( "Converting to H.265 MKV<ul><li>%1</li>to<li>%2</li></ul>" ).arg( getDispName( processInfo.fOldName ) ).arg( getDispName( processInfo.fNewNames.front() ) );
+            return retVal;
+        }
+
+        QStringList CMakeH265MKVModel::headers() const
+        {
+            return CDirModel::headers() << getMediaHeaders();
+            ;
+        }
+
+        void CMakeH265MKVModel::postLoad( QTreeView *treeView )
+        {
+            CDirModel::postLoad( treeView );
+        }
+
+        void CMakeH265MKVModel::preLoad( QTreeView *treeView )
+        {
+            CDirModel::preLoad( treeView );
+        }
+
+        void CMakeH265MKVModel::postProcess( bool /*displayOnly*/ )
+        {
+            if ( progressDlg() )
+                progressDlg()->setValue( 0 );
+        }
+
+        void CMakeH265MKVModel::postFileFunction( bool /*aOK*/, const QFileInfo & /*fileInfo*/, TParentTree & /*tree*/, bool /*countOnly*/ )
+        {
+        }
+
+        bool CMakeH265MKVModel::preFileFunction( const QFileInfo & /*fileInfo*/, std::unordered_set< QString > & /*alreadyAdded*/, TParentTree & /*tree*/, bool /*countOnly*/ )
+        {
+            return true;
+        }
+
+        void CMakeH265MKVModel::attachTreeNodes( QStandardItem * /*nextParent*/, QStandardItem *& /*prevParent*/, const STreeNode & /*treeNode*/ )
+        {
+        }
+
+    }
+}
---
+++ Models/MakeH265MKVModel.h
@@ -0,0 +1,65 @@
+// The MIT License( MIT )
+//
+// Copyright( c ) 2020-2021 Scott Aron Bloom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef _MAKEH265MKVMODEL_H
+#define _MAKEH265MKVMODEL_H
+
+#include "DirModel.h"
+
+namespace NMediaManager
+{
+    namespace NModels
+    {
+        class CMakeH265MKVModel : public CDirModel
+        {
+            Q_OBJECT
+        public:
+            enum EColumns
+            {
+                eMediaColumnLoc = NModels::EColumns::eFirstCustomColumn,
+            };
+
+            CMakeH265MKVModel( NUi::CBasePage *page, QObject *parent = nullptr );
+            virtual ~CMakeH265MKVModel() override;
+
+        private:
+            virtual QStringList dirModelFilter() const override;
+
+            virtual std::pair< bool, QStandardItem * > processItem( const QStandardItem *item, bool displayOnly ) override;
+            virtual bool showMediaItems() const override { return true; };
+            virtual int firstMediaItemColumn() const override { return EColumns::eMediaColumnLoc; }
+            virtual QStringList headers() const override;
+            virtual QString getProgressLabel( const SProcessInfo &processInfo ) const override;
+            virtual void postLoad( QTreeView * /*treeView*/ ) override;
+            virtual void preLoad( QTreeView * /*treeView*/ ) override;
+            virtual void postProcess( bool /*displayOnly*/ ) override;
+
+            virtual void postFileFunction( bool aOK, const QFileInfo &fileInfo, TParentTree & /*tree*/, bool countOnly ) override;
+            virtual bool preFileFunction( const QFileInfo &fileInfo, std::unordered_set< QString > &alreadyAdded, TParentTree &tree, bool countOnly ) override;
+
+            virtual void attachTreeNodes( QStandardItem * /*nextParent*/, QStandardItem *& /*prevParent*/, const STreeNode & /*treeNode*/ ) override;
+
+            virtual bool usesQueuedProcessing() const override { return true; }
+        };
+    }
+}
+#endif
