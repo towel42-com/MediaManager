8d7c2daa5d9d77d7a923c8797be3df0684067dc7
Scott Aron Bloom <scott@towel42.com>
2023-03-08 17:30:35 -0800

    Show audio sample rate, allow for non-mkv containers to be created

---
+++ Models/MakeMKVModel.cpp
@@ -0,0 +1,210 @@
+// The MIT License( MIT )
+//
+// Copyright( c ) 2020-2021 Scott Aron Bloom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "MakeMKVModel.h"
+#include "Preferences/Core/Preferences.h"
+#include "SABUtils/FileUtils.h"
+#include "SABUtils/DoubleProgressDlg.h"
+#include "SABUtils/MediaInfo.h"
+
+#include <QDir>
+#include <QTimer>
+
+namespace NMediaManager
+{
+    namespace NModels
+    {
+        CMakeMKVModel::CMakeMKVModel( NUi::CBasePage *page, QObject *parent /*= 0*/ ) :
+            CDirModel( page, parent )
+        {
+        }
+
+        CMakeMKVModel::~CMakeMKVModel()
+        {
+        }
+
+        std::optional< NMediaManager::NModels::TItemStatus > CMakeMKVModel::computeItemStatus( const QModelIndex &idx ) const
+        {
+            if ( isRootPath( idx ) )
+                return {};
+
+            auto mediaInfo = getMediaInfo( idx );
+            if ( !mediaInfo )
+                return {};
+
+            auto transCodeNeeded = NPreferences::NCore::CPreferences::instance()->getTranscodeNeeded( mediaInfo );
+            auto fileInfo = this->fileInfo( idx );
+
+            if ( idx.column() == 0 )   // filename
+            {
+                if ( std::get< 2 >( transCodeNeeded ) )
+                {
+                    auto msg = tr( "<p style='white-space:pre'>File <b>'%1'</b> is not using a %2 container</p>" ).arg( fileInfo.fileName() ).arg( NPreferences::NCore::CPreferences::instance()->getForceMediaFormatName() );
+                    return TItemStatus( NPreferences::EItemStatus::eWarning, msg );
+                }
+            }
+            else if ( idx.column() == getMediaVideoCodecLoc() )
+            {
+                if ( std::get< 0 >( transCodeNeeded ) )
+                {
+                    auto fileInfo = this->fileInfo( idx );
+                    auto msg = tr( "<p style='white-space:pre'>File <b>'%1'</b> is not using the H.265 video codec</p>" ).arg( fileInfo.fileName() );
+                    return TItemStatus( NPreferences::EItemStatus::eWarning, msg );
+                }
+            }
+            else if ( idx.column() == getMediaAudioCodecLoc() )
+            {
+                if ( std::get< 1 >( transCodeNeeded ) )
+                {
+                    auto fileInfo = this->fileInfo( idx );
+                    auto msg = tr( "<p style='white-space:pre'>File <b>'%1'</b> is not using the %2 audio codec</p>" ).arg( fileInfo.fileName() ).arg( NPreferences::NCore::CPreferences::instance()->getTranscodeToAudioCodec() );
+                    return TItemStatus( NPreferences::EItemStatus::eWarning, msg );
+                }
+            }
+            return {};
+        }
+
+        QStringList CMakeMKVModel::dirModelFilter() const
+        {
+            return NPreferences::NCore::CPreferences::instance()->getVideoExtensions();
+        }
+
+        std::pair< bool, QStandardItem * > CMakeMKVModel::processItem( const QStandardItem *item, bool displayOnly )
+        {
+            if ( item->data( ECustomRoles::eIsDir ).toBool() )
+                return std::make_pair( true, nullptr );
+
+            auto path = item->data( ECustomRoles::eAbsFilePath ).toString();
+
+            auto mediaInfo = getMediaInfo( path );
+
+            auto fi = QFileInfo( path );
+            auto transCodeNeeded = NPreferences::NCore::CPreferences::instance()->getTranscodeNeeded( mediaInfo );
+            if ( !std::get< 0 >( transCodeNeeded ) && !std::get< 1 >( transCodeNeeded ) && !std::get< 2 >( transCodeNeeded ) )
+                return std::make_pair( true, nullptr );
+
+            SProcessInfo processInfo;
+            processInfo.fSetMKVTagsOnSuccess = true;
+            processInfo.fOldName = path;
+
+            auto newBaseName = fi.completeBaseName() + "." + NPreferences::NCore::CPreferences::instance()->getForceMediaFormatExt();
+            if ( !std::get< 2 >( transCodeNeeded ) )
+                newBaseName += ".new";
+
+            processInfo.fNewNames << fi.absoluteDir().absoluteFilePath( newBaseName );
+
+            auto msg = tr( "Convert file from '%1' => '%2'" ).arg( getDispName( processInfo.fOldName ) ).arg( getDispName( processInfo.fNewNames.front() ) );
+            if ( std::get< 0 >( transCodeNeeded ) && std::get< 1 >( transCodeNeeded ) )
+                msg += tr( ", transcode into H.265, and transcode to %1." ).arg( NPreferences::NCore::CPreferences::instance()->getTranscodeToAudioCodec() );
+            else if ( std::get< 0 >( transCodeNeeded ) && !std::get< 1 >( transCodeNeeded ) )
+                msg += tr( " and transcode into H.265." );
+            else if ( !std::get< 0 >( transCodeNeeded ) && std::get< 1 >( transCodeNeeded ) )
+                msg += tr( " transcode to %1." ).arg( NPreferences::NCore::CPreferences::instance()->getTranscodeToAudioCodec() );
+
+            processInfo.fItem = new QStandardItem( msg );
+            processInfo.fItem->setData( processInfo.fOldName, ECustomRoles::eOldName );
+            processInfo.fItem->setData( processInfo.fNewNames, ECustomRoles::eNewName );
+
+            bool aOK = true;
+            QStandardItem *myItem = nullptr;
+            fFirstProcess = true;
+            if ( !displayOnly )
+            {
+                auto mediaInfo = getMediaInfo( fi );
+                Q_ASSERT( mediaInfo );
+
+                processInfo.fMaximum = mediaInfo->getNumberOfSeconds();
+                processInfo.fCmd = NPreferences::NCore::CPreferences::instance()->getFFMpegEXE();
+                if ( processInfo.fCmd.isEmpty() || !QFileInfo( processInfo.fCmd ).isExecutable() )
+                {
+                    QStandardItem *errorItem = nullptr;
+                    if ( processInfo.fCmd.isEmpty() )
+                        appendError( processInfo.fItem, tr( "ffmpeg is not set properly" ) );
+                    else
+                        appendError( processInfo.fItem, tr( "ffmpeg '%1' is not an executable" ).arg( processInfo.fCmd ) );
+                    aOK = false;
+                }
+
+                aOK = aOK && checkProcessItemExists( processInfo.fOldName, processInfo.fItem );
+                processInfo.fTimeStamps = NSABUtils::NFileUtils::timeStamps( processInfo.fOldName );
+
+                processInfo.fArgs = NPreferences::NCore::CPreferences::instance()->getTranscodeArgs( mediaInfo, processInfo.fOldName, processInfo.fNewNames.front() );
+
+                fProcessQueue.push_back( processInfo );
+                QTimer::singleShot( 0, this, &CDirModel::slotRunNextProcessInQueue );
+            }
+            myItem = processInfo.fItem;
+            return std::make_pair( aOK, myItem );
+        }
+
+        QString CMakeMKVModel::getProgressLabel( const SProcessInfo &processInfo ) const
+        {
+            auto retVal = QString( "Converting to H.265 %3<ul><li>%1</li>to<li>%2</li></ul>" ).arg( getDispName( processInfo.fOldName ) ).arg( getDispName( processInfo.fNewNames.front() ) ).arg( NPreferences::NCore::CPreferences::instance()->getForceMediaFormatName() );
+            return retVal;
+        }
+
+        QStringList CMakeMKVModel::headers() const
+        {
+            return CDirModel::headers() << getMediaHeaders();
+            ;
+        }
+
+        void CMakeMKVModel::postLoad( QTreeView *treeView )
+        {
+            CDirModel::postLoad( treeView );
+        }
+
+        void CMakeMKVModel::preLoad( QTreeView *treeView )
+        {
+            CDirModel::preLoad( treeView );
+        }
+
+        void CMakeMKVModel::postProcess( bool /*displayOnly*/ )
+        {
+            if ( progressDlg() )
+                progressDlg()->setValue( 0 );
+        }
+
+        void CMakeMKVModel::postFileFunction( bool /*aOK*/, const QFileInfo & /*fileInfo*/, TParentTree & /*tree*/, bool /*countOnly*/ )
+        {
+        }
+
+        bool CMakeMKVModel::preFileFunction( const QFileInfo &fileInfo, std::unordered_set< QString > & /*alreadyAdded*/, TParentTree & /*tree*/, bool /*countOnly*/ )
+        {
+            static bool showAll = false;
+#if defined( DEBUG ) && defined( MAKEMKVMODEL_SHOWALL )
+            showAll = true;
+#endif;
+            if ( showAll )
+                return true;
+
+            auto mediaInfo = getMediaInfo( fileInfo );
+            auto transCodeNeeded = NPreferences::NCore::CPreferences::instance()->getTranscodeNeeded( mediaInfo );
+            return std::get< 0 >( transCodeNeeded ) || std::get< 1 >( transCodeNeeded ) || std::get< 2 >( transCodeNeeded );
+        }
+
+        void CMakeMKVModel::attachTreeNodes( QStandardItem * /*nextParent*/, QStandardItem *& /*prevParent*/, const STreeNode & /*treeNode*/ )
+        {
+        }
+
+    }
+}
---
+++ UI/MakeMKVPage.cpp
@@ -0,0 +1,124 @@
+// The MIT License( MIT )
+//
+// Copyright( c ) 2020-2021 Scott Aron Bloom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "MakeMKVPage.h"
+
+#include "Preferences/Core/Preferences.h"
+#include "Models/MakeMKVModel.h"
+#include "SABUtils/DoubleProgressDlg.h"
+
+#include <QRegularExpression>
+
+namespace NMediaManager
+{
+    namespace NUi
+    {
+        CMakeMKVPage::CMakeMKVPage( QWidget *parent ) :
+            CBasePage( "Make H.265 MKV", parent )
+        {
+        }
+
+        CMakeMKVPage::~CMakeMKVPage()
+        {
+        }
+
+        NModels::CDirModel *CMakeMKVPage::createDirModel()
+        {
+            return new NModels::CMakeMKVModel( this );
+        }
+
+        QString CMakeMKVPage::secondaryProgressLabel() const
+        {
+            return tr( "Current (seconds):" );
+        }
+
+        QString CMakeMKVPage::loadTitleName() const
+        {
+            return tr( "Finding Files" );
+        }
+
+        QString CMakeMKVPage::loadCancelName() const
+        {
+            return tr( "Cancel" );
+        }
+
+        QString CMakeMKVPage::actionTitleName() const
+        {
+            return tr( "Creating H.265 MKV..." );
+        }
+
+        QString CMakeMKVPage::actionCancelName() const
+        {
+            return tr( "Abort Creating H.265 MKV" );
+        }
+
+        QString CMakeMKVPage::actionErrorName() const
+        {
+            return tr( "Error while Creating H.265 MKV:" );
+        }
+
+        void CMakeMKVPage::postProcessLog( const QString &string )
+        {
+            // time=00:00:00.00
+            auto regEx = QRegularExpression( "[Tt]ime\\=\\s*(?<hours>\\d{2}):(?<mins>\\d{2}):(?<secs>\\d{2})" );
+
+            auto pos = string.lastIndexOf( regEx );
+            if ( pos == -1 )
+                return;
+
+            auto match = regEx.match( string, pos );
+            if ( !match.hasMatch() )
+                return;
+
+            auto hours = match.captured( "hours" );
+            auto mins = match.captured( "mins" );
+            auto secs = match.captured( "secs" );
+
+            int numSeconds = 0;
+            if ( !hours.isEmpty() )
+            {
+                bool aOK;
+                int curr = hours.toInt( &aOK );
+                if ( aOK )
+                    numSeconds += curr * 60 * 60;
+            }
+
+            if ( !mins.isEmpty() )
+            {
+                bool aOK;
+                int curr = mins.toInt( &aOK );
+                if ( aOK )
+                    numSeconds += curr * 60;
+            }
+
+            if ( !secs.isEmpty() )
+            {
+                bool aOK;
+                int curr = secs.toInt( &aOK );
+                if ( aOK )
+                    numSeconds += curr;
+            }
+
+            fProgressDlg->setSecondaryValue( numSeconds );
+        }
+    }
+}
